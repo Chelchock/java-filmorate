7c6a130cbe95ed973ee5b490470440cc5ea0559a
Chelchock <nigmatzianow.marsel@gmail.com>
2026-06-11 15:21:09 +0300

    fix: Исправление файлов

--- src/main/java/ru/yandex/practicum/filmorate/handler/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/GlobalExceptionHandler.java
@@ -2,12 +2,16 @@ package ru.yandex.practicum.filmorate.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.ApiErrorResponse;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -20,17 +24,29 @@ public class GlobalExceptionHandler {
         return new ApiErrorResponse(ex.getMessage());
     }
 
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiErrorResponse handleNotFound(NotFoundException ex) {
-        log.warn("Ресурс не найден: {}", ex.getMessage());
-        return new ApiErrorResponse(ex.getMessage());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining("; "));
+        log.warn("Ошибка валидации аргументов: {}", errorMessage);
+        return new ApiErrorResponse(errorMessage);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErrorResponse handleIllegalArgument(IllegalArgumentException ex) {
         log.warn("Некорректный аргумент: {}", ex.getMessage());
+        return new ApiErrorResponse(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErrorResponse handleNotFound(NotFoundException ex) {
+        log.warn("Ресурс не найден: {}", ex.getMessage());
         return new ApiErrorResponse(ex.getMessage());
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,23 +1,29 @@
 package ru.yandex.practicum.filmorate.service;
 
-import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Collection;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FilmService {
     private final FilmStorage filmStorage;
+    private final UserStorage userStorage;
 
     public Film create(Film film) {
+        log.debug("Создание фильма: {}", film);
         return filmStorage.create(film);
     }
 
     public Film update(Film film) {
+        log.debug("Обновление фильма: {}", film);
         return filmStorage.update(film);
     }
 
@@ -30,10 +36,16 @@ public class FilmService {
     }
 
     public void addLike(Long filmId, Long userId) {
+        log.debug("Добавление лайка: filmId={}, userId={}", filmId, userId);
+        filmStorage.findById(filmId);
+        userStorage.findById(userId);
         filmStorage.addLike(filmId, userId);
     }
 
     public void removeLike(Long filmId, Long userId) {
+        log.debug("Удаление лайка: filmId={}, userId={}", filmId, userId);
+        filmStorage.findById(filmId);
+        userStorage.findById(userId);
         filmStorage.removeLike(filmId, userId);
     }
 
